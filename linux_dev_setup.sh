cd1c391b494a63ac3e3307b3dfc14f2b243c5c02
Larysson Alves <larysson.alves@gmail.com>
2022-04-29 18:22:08 -0300

    dotfiles clonning

--- linux_dev_setup.sh
+++ linux_dev_setup.sh
@@ -1,19 +1,23 @@
-PURPLE='\033[0;35m'
+PURPLE='\033[0;35m' # Purple
 NC='\033[0m' # No Color
 
 function output() {
   echo -e "${PURPLE}$1${NC}"
 }
 
-output "Installing -> git"
+output "Installing -> git curl"
 sudo apt install git -y
+sudo apt install curl -y
 
 output "Installing -> oh-my-bash"
 curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh | bash
-source $HOME/.profile
 
 output "Installing -> nvm"
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+output "Sourcing -> .profile .bashrc"
+source $HOME/.profile
+source $HOME/.bashrc
 
 output "Installing -> node yarn"
 nvm install --lts
@@ -24,5 +28,7 @@ sudo apt install python3
 sudo apt install python3-venv -y
 sudo apt install python3-dev -y
 
-output "Clonning dotfiles at ${HOME}"
-git clone https://github.com/laryssonalves/dotfiles.git $HOME
+DOTFILES_PATH=$HOME/.dotfiles
+
+output "Clonning dotfiles at ${DOTFILES_PATH}"
+git clone https://github.com/laryssonalves/.dotfiles.git $DOTFILES_PATH
