b3711aa403a0e7f3177e91618c5ddc712b3ce231
Larysson Alves <larysson.alves@gmail.com>
2022-05-01 00:00:24 -0300

    removed sourcing inside shell script

--- linux_dev_setup.sh
+++ linux_dev_setup.sh
@@ -1,41 +1,42 @@
-PURPLE='\033[0;35m' # Purple
-NC='\033[0m'        # No Color
-DOTFILES_PATH=$HOME/.dotfiles
-BASHRC_PATH=$HOME/.bashrc
+PURPLE="\033[0;35m" # Purple
+NC="\033[0m"        # No Color
+DOTFILES_PATH=~/.dotfiles # Repo path
+BASHRC_PATH=~/.profile # Bash config path
 
 function output() {
-  echo -e '${PURPLE}$1${NC}'
+  echo -e "${PURPLE}$1${NC}"
 }
 
-output 'Installing -> git curl'
+output "Installing -> git curl"
 sudo apt install git -y
 sudo apt install curl -y
 
-output 'Installing -> oh-my-bash'
+output "Installing -> oh-my-bash"
 curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh | bash
 
-output 'Installing -> nvm'
+output "Installing -> nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-output 'Clonning dotfiles at ${DOTFILES_PATH}'
+output "Clonning dotfiles at $DOTFILES_PATH"
+if [ -d "$DOTFILES_PATH" ]; then
+    sudo rm -rf $DOTFILES_PATH
+fi
+
 git clone https://github.com/laryssonalves/.dotfiles.git $DOTFILES_PATH
 
-output 'Installing -> aliases'
+output "Installing -> aliases"
+echo -e "\n# Adding my custom aliases" | tee -a $BASHRC_PATH
+echo -e "source $DOTFILES_PATH/linux_aliases.sh" | tee -a $BASHRC_PATH
 
-echo -e "\nsource $DOTFILES_PATH/linux_aliases.sh" | tee -a $BASHRC_PATH
-
-output 'Sourcing -> .bashrc'
-source $BASHRC_PATH
-
-output 'Installing -> node yarn'
+output "Installing -> node yarn"
 nvm install --lts
 sudo apt install yarn -y
 
-output 'Installing -> python and libs'
+output "Installing -> python and libs"
 sudo apt install python3
 sudo apt install python3-venv -y
 sudo apt install python3-dev -y
 
-output 'Installing -> postgresql'
+output "Installing -> postgresql"
 sudo apt install postgresql
 sudo service postgresql start
