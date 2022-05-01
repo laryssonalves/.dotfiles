4426e565290671f392e9e40914d5a49c77e0e960
Larysson Alves <larysson.alves@gmail.com>
2022-04-30 23:10:08 -0300

    feat(aliases): added more aliases

--- linux_aliases.sh
+++ linux_aliases.sh
@@ -1,1 +1,16 @@
-alias bashconfig='nano ~/.bashrc'
+# General aliases
+alias bashconfig="nano ~/.bashrc"
+alias h="history"
+alias hg="history | grep"
+alias myip="curl http://ipecho.net/plain; echo"
+alias update="sudo apt update && sudo apt upgrade"
+
+# Dev aliases
+alias start-postgres="sudo service postgresql start"
+alias restart-postgres="sudo service postgresql restart"
+alias nis="npm install --save"
+alias nig="npm install -g"
+alias yi="yarn install"
+alias ya="yarn add"
+alias yad="yarn add -D"
+alias gac="git add . && git commit -a -m"
